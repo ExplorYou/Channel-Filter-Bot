c74c3aad62eaec7ab8a2c8c4db24575e23c437c1
ExplorYou <157680290+ExplorYou@users.noreply.github.com>
2024-08-15 11:25:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+API_ID       = 28596507
+API_HASH     = "f3bc3f6d6829d5f1d7213c5a3c92a5ca"
+BOT_TOKEN    = "6908156534:AAE1sbST0SCqOvo1LJr5XINQ1RdPx3osAYY"
+SESSION      = "BQG0WRsAeLlwcFonf3Fzam_a6bfzKuMpbD6b2pac3_WmPA8T51Dv2Q2ESD3iHxM0hoek7me6zXBn5SH2P6aKVQ0pb40BlvNrNBolBD1o_ZRF6xbpjK_BsXXrYUBtWgN_mcMBFDenekESDqentLJVSCiFVVuAS6MOODDg2MqqiaqIIju-9BYWwbEEIWIfvruZWpsOZgYH7OAir7fovJeeEnbRIHLqbtvcsnADYQJ0uYY1UiAv10bASo8U22fWK0FC833rxIbgUuCKSUh_6pwV_Hg8RppRvavNrrnZUvwR3jg_d276C8wEJPDm3z9mL_7JuZeDGYjbmEFqtEpDcsqmq1HE4BKXOAAAAAGbwhp2AQ"
+DATABASE_URI = "mongodb+srv://movie93268:5Kza8A6z0mEqrKuF@nowtesttrybot.xhkmb.mongodb.net/?retryWrites=true&w=majority&appName=nowtesttrybot"
+LOG_CHANNEL  = -1002206453914
+ADMIN        = 5733456548
+CHANNEL      = "@req_fullfil @Sup_Toon_1"
